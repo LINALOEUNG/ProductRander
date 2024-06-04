9a5d2c11f309e4dbf9c0edc09d7441a01f328e69
LOEUNG LINA <linag8296@gmail.com>
2024-06-04 11:48:03 +0700

    frist Commit API

--- components/CardComnetjson.js
+++ components/CardComnetjson.js
@@ -1,21 +1,33 @@
-export const CardComponet = (productjson) => {
+// import { product } from "../data/product";
+
+// export const CardComponetjson =(product) => {
+//     const {
+//         id ,
+//         name ,
+//         price : {maxPrice}
+//     } = product ;
+//     return 
+    
+// }
+export const CardComponetjson = (productjson) => {
     const {
-      image,
-      title,
-      price,
-      rating: { rate }
-    } = productjson ;
+        mainImage,
+      name,
+      stock,
+      price: {maxPrice}
+      
+    } = productjson;
     return `
   <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
   <a href="#">
       <img class="p-8 rounded-t-lg h-[200px] w-auto object-cover" src="${
-        image || "../assets/placeholder-1.webp"
-      }" alt="product image" />
+        mainImage || ""
+      }" alt="productjson image" />
   </a>
   <div class="px-5 pb-5">
       <a href="#">
           <h5 class="line-clamp-1 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${
-            title || "No product title"
+            name || "No product title"
           }</h5>
       </a>
       <div class="flex items-center mt-2.5 mb-5">
@@ -37,12 +49,12 @@ export const CardComponet = (productjson) => {
               </svg>
           </div>
           <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">${
-            rate || 0
-          }</span>
+            maxPrice}
+          </span>
       </div>
       <div class="flex items-center justify-between">
           <span class="text-1xl font-bold text-gray-900 dark:text-white">${
-            price || "Unavailable"
+            stock || "Unavailable"
           } $</span>
           <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
       </div>
